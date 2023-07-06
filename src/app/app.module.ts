899205f58e1204497c7448070062b3b3cc19df29
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-06 10:31:40 +0200

    componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfessionalsComponent } from './pages/professionals/professionals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProfessionalsComponent
   ],
   imports: [
     BrowserModule,
